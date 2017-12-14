1520096ac71b2b5c3a56a3ea6a17f7052ce8189d
Joe Simon <simonjm09@gmail.com>
2017-12-13 18:35:06 -0600

    Completed day 6 part 2

--- 2017/6/6.fsx
+++ 2017/6/6.fsx
@@ -2,41 +2,60 @@ open System
 open System.Collections.Generic
 open System.IO
 
+let toStr (banks:int[]) = String.Concat banks
+
 let redistribute (banks:int[]) start =
     let len = banks.Length
     let num = banks.[start]
     banks.[start] <- 0
     for i = 1 to num do
-        // printfn "%d -> %A" i banks
         let index = (start + i) % len
         let cur = banks.[index]
         banks.[index] <- cur + 1
 
+let cache = Dictionary<string, int>()
 let pickIndex (banks:int[]) =
-    let result = 
-        banks
-        |> Seq.mapi (fun i n -> (i, n))
-        |> Seq.sortWith (fun (i1, n1) (i2, n2) -> 
-            let result = n2 - n1 
-            if result = 0 then i1 - i2 else result
-        )
-        |> Seq.head
-    fst result
+    let str = toStr banks
+    match cache.TryGetValue str with
+    | (true, result) -> result
+    | (false, _) ->
+        let (index, _) = 
+            banks
+            |> Seq.mapi (fun i n -> (i, n))
+            |> Seq.sortWith (fun (i1, n1) (i2, n2) -> 
+                let result = n2 - n1 
+                if result = 0 then i1 - i2 else result
+            )
+            |> Seq.head
+        cache.Add(str, index)
+        index
 
-let visited = HashSet<string>()
-let hasVisited (banks:int[]) = 
-    let str = String.Concat banks
-    not <| visited.Add str
+let hasVisited (visited:HashSet<string>) (banks:int[]) = 
+    not <| (visited.Add << toStr) banks
 
 let input = File.ReadAllText("input.txt").Split('\t')
 
 let run banks =
+    let visited = hasVisited (HashSet<string>())
     let total =
         Seq.initInfinite id
         |> Seq.filter (fun _ ->
             let start = pickIndex banks
             redistribute banks start
-            hasVisited banks
+            visited banks
+        )
+        |> Seq.head
+    total + 1
+
+let run2 banks =
+    run banks |> ignore
+    let startingState = Array.copy banks
+    let total =
+        Seq.initInfinite id
+        |> Seq.filter (fun _ ->
+            let start = pickIndex banks
+            redistribute banks start
+            banks = startingState
         )
         |> Seq.head
     total + 1
@@ -46,4 +65,5 @@ let banks =
     |> Seq.map int
     |> Seq.toArray
 
-printfn "Part 1 = %d" <| (run <| Array.copy banks)
+printfn "Part 1 = %d" <| (run << Array.copy) banks
+printfn "Part 2 = %d" <| (run2 << Array.copy) banks
