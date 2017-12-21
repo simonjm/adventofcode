61763d57f6d21eb5aba6ae094fba8e2f780905f7
Joe Simon <simonjm09@gmail.com>
2017-12-21 17:49:33 -0600

    Completed day 9 part 2

--- 2017/9/9.fsx
+++ 2017/9/9.fsx
@@ -5,7 +5,9 @@ type State = {
     IgnoreMode: bool;
     Score: int;
     OpenGroup: int;
+    GarbageCount: int
 }
+
 let folder (prev:State) cur =
     match cur with
     | _ when prev.IgnoreMode -> { prev with IgnoreMode = false }
@@ -13,13 +15,15 @@ let folder (prev:State) cur =
     | '!' when not prev.IgnoreMode -> { prev with IgnoreMode = true }
     | '!' -> prev
     | '>' when prev.InGarbage -> { prev with InGarbage = false }
-    | _ when prev.InGarbage -> prev
+    | _ when prev.InGarbage -> { prev with GarbageCount = prev.GarbageCount + 1 }
     | '{' -> { prev with OpenGroup = prev.OpenGroup + 1 }
     | '}' when prev.OpenGroup > 0 -> { prev with OpenGroup = prev.OpenGroup - 1; Score = prev.Score + prev.OpenGroup }
     | _ -> prev
-let initialState = { InGarbage = false; IgnoreMode = false; Score = 0; OpenGroup = 0 }
+
+let initialState = { InGarbage = false; IgnoreMode = false; Score = 0; OpenGroup = 0; GarbageCount = 0 }
 let state =
     File.ReadAllText "input.txt"
     |> Seq.fold folder initialState
 
 printfn "Part 1 = %d" state.Score
+printfn "Part 2 = %d" state.GarbageCount
