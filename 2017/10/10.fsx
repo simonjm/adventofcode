014616b7858c83a5f750c836a4a1ba18d52775f1
Joe Simon <simonjm09@gmail.com>
2017-12-26 10:39:45 -0600

    Completed day 10 part 2

--- 2017/10/10.fsx
+++ 2017/10/10.fsx
@@ -1,37 +1,68 @@
+open System
 open System.IO
+open System.Text
 
 type State = {
     Current: int
     Skip: int
 }
 
-let values = [| for i in 0 .. 255 -> i |]
-// let values = [| for i in 0 .. 4 -> i |]
+let hash (values:int[]) (lengths:seq<int>) rounds = 
+    let folder (prev:State) length =
+        let start = prev.Current 
+        let last = start + length
 
-let folder (prev:State) length =
-    let start = prev.Current 
-    let last = start + length
+        // reverse the sublist
+        let newList = [
+            for i = last - 1 downto start do
+                yield values.[i % values.Length]
+        ]
 
-    // reverse the sublist
-    let newList = [
-        for i = last - 1 downto start do
-            yield values.[i % values.Length]
-    ]
+        // apply the results to the array
+        newList 
+        |> List.fold (fun i v -> 
+            values.[i % values.Length] <- v
+            i + 1
+        ) start
+        |> ignore
+        { Current = (last + prev.Skip) % values.Length; Skip = prev.Skip + 1 }
 
-    // apply the results to the array
-    newList 
-    |> List.fold (fun i v -> 
-        values.[i % values.Length] <- v
-        i + 1
-    ) start
-    |> ignore
-    { Current = (last + prev.Skip) % values.Length; Skip = prev.Skip + 1 }
+    let mutable state = { Current = 0; Skip = 0 }
+    for i = 1 to rounds do
+        state <- Seq.fold folder state lengths
 
-let initialState = { Current = 0; Skip = 0 }
+let values = [ for i in 0 .. 255 -> i ]
+let input = File.ReadAllText("input.txt")
 
-File.ReadAllText("input.txt").Split(',')
-|> Seq.map int
-// [3; 4; 1; 5]
-|> Seq.fold folder initialState
+let toInts (strings:string) = 
+    strings.Split([|','|], StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map int
 
-printfn "Part 1 = %d" <| values.[0] * values.[1]
+let reduceHash (hash:int[]) = 
+    hash
+    |> Seq.chunkBySize 16
+    |> Seq.map (Array.reduce (^^^))
+    |> Seq.map (fun i -> i.ToString("X").PadLeft(2, '0'))
+    |> String.Concat
+
+let part1 (value:string) =
+    let valuesCopy = List.toArray values
+    let lengths = toInts value
+    hash valuesCopy lengths 1
+    valuesCopy.[0] * valuesCopy.[1]
+
+let part2 (value:string) =
+    let suffix = "17,31,73,47,23"
+    let bytes = Encoding.ASCII.GetBytes value
+    let ascii = String.Join(",", bytes)
+    let lengths = 
+        String.Join(",", ascii, suffix)
+        |> toInts
+        |> Seq.toList
+
+    let valuesCopy = List.toArray values
+    hash valuesCopy lengths 64
+    reduceHash valuesCopy
+
+printfn "Part 1 = %d" <| part1 input
+printfn "Part 2 = %s" <| part2 input
