e30c2104a6999632722e45a3794c19e58b9486c0
Joe Simon <simonjm09@gmail.com>
2018-12-04 20:45:14 -0600

    Completed day 1

--- 2018/1/1.fsx
+++ 2018/1/1.fsx
@@ -1,4 +1,7 @@
 open System.IO;
+open System.Collections.Generic;
+open System.Collections.Generic
+open System.Data
 
 type Op =
     | Inc of int
@@ -16,11 +19,23 @@ let execOp total op =
     | Inc num -> total + num 
     | Dec num -> total - num
 
-let lines = File.ReadAllLines "input.txt"
+let lines = File.ReadLines "input.txt" |> Seq.toList
 
-let processLines start =
+let part1() =
     lines
     |> Seq.map parseLine
-    |> Seq.fold execOp start
+    |> Seq.fold execOp 0
 
-processLines 0 |> printfn "Part 1 = %d"
+let part2() =
+    let set = HashSet<int>()
+    let rec loop list acc =
+        match list with
+        | [] -> loop lines acc
+        | h :: t -> 
+            let total = parseLine h |> execOp acc
+            if set.Add(total) then loop t total
+            else total
+    loop lines 0
+
+part1() |> printfn "Part 1 = %d"
+part2() |> printfn "Part 2 = %d"
